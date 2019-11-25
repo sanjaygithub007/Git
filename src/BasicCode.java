c004ea5e7b1d9a4230ceb64d03a863645eda6b35
Sanjay Singh <sanjaysinghjsg@gmail.com>
2019-11-25 11:42:43 +0530

    Initial commit

---
+++ src/BasicCode.java
@@ -0,0 +1,18 @@
+
+public class BasicCode {
+	
+	public static void main(String[] args) {
+		int sum=add(4,5);
+		int substract=sub(10,5);
+	}
+	
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+
+	public static int sub(int a, int b)
+	{
+		return a-b;
+	}
+}
